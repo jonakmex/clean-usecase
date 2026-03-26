59eb6d9e6bca7311b7759a42f209dfaf35440f77
Jonathan Gomez <jonakmex@gmail.com>
2026-03-25 21:56:42 -0600

    feat: QuerybalanceUseCase Implementation

--- src/main/java/com/usecase/model/response/Response.java
+++ src/main/java/com/usecase/model/response/Response.java
@@ -1,8 +1,10 @@
 package com.usecase.model.response;
 
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @SuperBuilder
+@NoArgsConstructor
 public class Response {
     public String status;
     public String error;
